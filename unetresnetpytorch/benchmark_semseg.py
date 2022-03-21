270075ec36d9ed02b4ea3707b52f95e23bc00c55
Vu Duc Anh <67143004+vuducanh0802@users.noreply.github.com>
2022-03-21 16:22:36 +0800

    Create benchmark_semseg.py

---
+++ unetresnetpytorch/benchmark_semseg.py
@@ -0,0 +1,175 @@
+import segmentation_models_pytorch as smp
+import utils
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as BaseDataset
+
+import numpy as np
+import os
+import argparse
+# !pip install git+https://github.com/qubvel/segmentation_models
+
+class Dataset(BaseDataset):
+    """CamVid Dataset. Read images, apply augmentation and preprocessing transformations.
+
+    Args:
+        images_dir (str): path to images folder
+        masks_dir (str): path to segmentation masks folder
+        class_values (list): values of classes to extract from segmentation mask
+        augmentation (albumentations.Compose): data transfromation pipeline
+            (e.g. flip, scale, etc.)
+        preprocessing (albumentations.Compose): data preprocessing
+            (e.g. noralization, shape manipulation, etc.)
+
+    """
+
+    CLASSES = ['sky', 'building', 'pole', 'road', 'pavement',
+               'tree', 'signsymbol', 'fence', 'car',
+               'pedestrian', 'bicyclist', 'unlabelled']
+
+    def __init__(
+            self,
+            images_dir,
+            masks_dir,
+            classes=None,
+            augmentation=None,
+            preprocessing=None,
+    ):
+        self.ids = os.listdir(images_dir)
+        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
+        self.masks_fps = [os.path.join(masks_dir, image_id) for image_id in self.ids]
+
+        # convert str names to class values on masks
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
+
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+
+    def __getitem__(self, i):
+
+        # read data
+        image = cv2.imread(self.images_fps[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.masks_fps[i], 0)
+
+        # extract certain classes from mask (e.g. cars)
+        masks = [(mask == v) for v in self.class_values]
+        mask = np.stack(masks, axis=-1).astype('float')
+
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+
+        return image, mask
+
+    def __len__(self):
+        return len(self.ids)
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
+parser = argparse.ArgumentParser(description="Testing Benchmarking")
+parser.add_argument("--model", type=str, metavar='', required=True, help='name of the model')
+parser.add_argument("--dataset", type=dir_path, metavar='', required=True, help='path of the dataset folder')
+args = parser.parse_args()
+
+def train(backbone, DATA_DIR):
+    ENCODER = backbone
+    ENCODER_WEIGHTS = 'imagenet'
+    CLASSES = ['car']
+    ACTIVATION = 'sigmoid'
+    DEVICE = 'cuda'
+
+    x_train_dir = os.path.join(DATA_DIR, 'train')
+    y_train_dir = os.path.join(DATA_DIR, 'trainannot')
+
+    x_valid_dir = os.path.join(DATA_DIR, 'val')
+    y_valid_dir = os.path.join(DATA_DIR, 'valannot')
+
+    x_test_dir = os.path.join(DATA_DIR, 'test')
+    y_test_dir = os.path.join(DATA_DIR, 'testannot')
+
+    # create segmentation model with pretrained encoder
+    model = smp.Unet(
+        encoder_name=ENCODER,
+        encoder_weights=ENCODER_WEIGHTS,
+        classes=len(CLASSES),
+        activation=ACTIVATION,
+        in_channels=1,
+    )
+
+    preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+
+    train_dataset = Dataset(
+        x_train_dir,
+        y_train_dir,
+        preprocessing=utils.get_preprocessing(preprocessing_fn),
+        classes=CLASSES,
+    )
+
+    valid_dataset = Dataset(
+        x_valid_dir,
+        y_valid_dir,
+        augmentation=utils.get_validation_augmentation(),
+        preprocessing=utils.get_preprocessing(preprocessing_fn),
+        classes=CLASSES,
+    )
+
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=12)
+    valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
+
+    loss = smp.utils.losses.DiceLoss()
+    metrics = [
+        smp.utils.metrics.IoU(threshold=0.5),
+    ]
+
+    optimizer = torch.optim.Adam([
+        dict(params=model.parameters(), lr=0.0001),
+    ])
+
+    train_epoch = smp.utils.train.TrainEpoch(
+        model,
+        loss=loss,
+        metrics=metrics,
+        optimizer=optimizer,
+        device=DEVICE,
+        verbose=True,
+    )
+
+    valid_epoch = smp.utils.train.ValidEpoch(
+        model,
+        loss=loss,
+        metrics=metrics,
+        device=DEVICE,
+        verbose=True,
+    )
+
+    max_score = 0
+
+    for i in range(0, 100):
+
+        print('\nEpoch: {}'.format(i))
+        train_logs = train_epoch.run(train_loader)
+        valid_logs = valid_epoch.run(valid_loader)
+
+        if max_score < valid_logs['iou_score']:
+            max_score = valid_logs['iou_score']
+            torch.save(model, './best_model.pth')
+            print('Model saved!')
+
+        if i == 25:
+            optimizer.param_groups[0]['lr'] = 1e-5
+            print('Decrease decoder learning rate to 1e-5!')
+
+if __name__ == '__main__':
+    train(args.model, args.dataset)
